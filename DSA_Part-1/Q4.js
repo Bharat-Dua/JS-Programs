0911a0f3e14bbc0a21ccaa63f024971bed9b4b23
Bharat Dua <bharatdua13@gmail.com>
2024-08-04 12:05:50 +0530

    solved: Write a for loop that prints the numbers 1 to 10.

---
+++ DSA_Part-1/Q4.js
@@ -0,0 +1,10 @@
+//* Write a for loop that prints the numbers 1 to 10. *//
+
+// method 1
+for (let i = 1; i <= 10; i++) console.log(i);
+// method 2
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
