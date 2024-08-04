5fb0573ba5b533ecb4ae1d0f318ba7c86012cba1
Bharat Dua <bharatdua13@gmail.com>
2024-08-04 10:06:09 +0530

    solved: Write an if statement that checks if a number is greater than 5.

---
+++ DSA_Part-1/Q1.js
@@ -0,0 +1,11 @@
+//* Write an if statement that checks if a number is greater than 5. *//
+
+let a = 5;
+// method 1
+if (a > 5) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+// method 2
+console.log(a > 5);
